8ff5c6a3739de7003d01181f2de232806cd2eddf
Russ Perry <rperry1886@gmail.com>
2019-03-02 12:36:57 -0500

    Make stats go up when pet transforms

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import random
 
+
 # Other Variables
 game_running = True
 
@@ -13,15 +14,42 @@ strength = 1
 happiness = 0
 poo = False
 
+# stages
+rookie = False
+champ = False
+ultimate = False
+mega = False
+
 # Poo Function
 def do_you_poo():
-    "Random number for pooping"
     poo_chance = [1, 2, 3, 4, 5, 6]
     does_poo = random.choice(poo_chance)
-    if does_poo == 1 or does_poo == 3 or does_poo == 5:
+    if does_poo == 1 or does_poo == 3:
+        global poo
         poo = True
         print("\nEw a pooie!")
-        return
+    return
+
+
+def level_up():
+    stat_chance = [0, 1, 2, 3, 4, 5]
+    global health
+    global max_stamina
+    global weight
+    global strength
+    health_up = random.choice(stat_chance)
+    stamina_up = random.choice(stat_chance)
+    weight_up = random.choice(stat_chance)
+    strength_up = random.choice(stat_chance)
+    print("Health + " + str(health_up))
+    print("Stamina + " + str(stamina_up))
+    print("Weight + " + str(weight_up))
+    print("Strength + " + str(strength_up) + "\n")
+    health += health_up
+    max_stamina += stamina_up
+    weight += weight_up
+    strength += strength_up
+    return
 
 
 print("Hello, meet your digimon partner, Viximon!")
@@ -82,14 +110,22 @@ while game_running:
     elif choice == "q" or choice == "Q":
         game_running = False
 
-    if strength >= 15 and weight >= 8 and weight <= 12 and happiness >= 20:
+    if strength >= 15 and 8 <= weight <= 12 and happiness >= 20 and rookie == False:
         print("\nWoah! " + digi_name + " transformed into a Renamon!\n")
-    if strength >= 30 and weight >= 20 and weight <= 22 and happiness >= 40:
+        rookie = True
+        level_up()
+    if strength >= 30 and 20 <= weight <= 22 and happiness >= 40 and champ == False:
         print("\nCongratulations! " + digi_name + " transformed into a Kyubimon!\n")
-    if strength >= 60 and weight >= 35 and weight <= 38 and happiness >= 75 and not poo:
+        champ = True
+        level_up()
+    if strength >= 60 and 30 <= weight <= 35 and happiness >= 75 and not poo and ultimate == False:
         print("\nHoly Cow! " + digi_name + " transformed into a Taomon!\n")
-    if strength >= 100 and weight == 50 and happiness >= 100 and not poo:
+        ultimate = True
+        level_up()
+    if strength >= 100 and weight == 50 and happiness >= 100 and not poo and mega == False:
         print("\nYou are the best handler! " + digi_name + " transformed into the highest level Sakuyamon!\n")
+        mega = True
+        level_up()
 
 
 
